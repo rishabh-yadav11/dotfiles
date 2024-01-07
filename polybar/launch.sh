f46d63e090a07d118410e67225537388f757dee3
rishabh-ayadav11 <rishabhyadav621@gmail.com>
2024-01-07 19:22:20 +0530

    intial

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#killall -q polybar
+
+# Launch bar1 and bar2
+#polybar  & disown
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main -c $(dirname $0)/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+	polybar external -c $(dirname $0)/config.ini &
+fi
---
+++ polybar/scripts/power.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+PATH_AC="/sys/class/power_supply/AC0"
+PATH_BATTERY_0="/sys/class/power_supply/BAT0"
+
+ac=0
+battery_level_0=0
+battery_max_0=0
+
+if [ -f "$PATH_AC/online" ]; then
+    ac=$(cat "$PATH_AC/online")
+ 
+fi
+
+if [ -f "$PATH_BATTERY_0/charge_now" ]; then
+    battery_level_0=$(cat "$PATH_BATTERY_0/charge_now")
+    
+fi
+
+if [ -f "$PATH_BATTERY_0/charge_full" ]; then
+    battery_max_0=$(cat "$PATH_BATTERY_0/charge_full")
+    
+fi
+
+battery_percent=$(($battery_level_0 * 100))
+battery_percent=$(($battery_percent / $battery_max_0))
+
+
+if [ "$ac" -eq 1 ]; then
+
+    if [ "$battery_percent" -gt 97 ]; then
+        echo "  100%"
+    else
+        echo "󰚥  $battery_percent%"
+    fi
+else
+    if [ "$battery_percent" -gt 90 ]; then
+        icon="󰂂"
+    elif [ "$battery_percent" -gt 80 ]; then
+        icon="󰂁"
+    elif [ "$battery_percent" -gt 70 ]; then
+        icon="󰂀"
+    elif [ "$battery_percent" -gt 60 ]; then
+        icon="󰁿"
+    elif [ "$battery_percent" -gt 50 ]; then
+        icon="󰁾"
+    elif [ "$battery_percent" -gt 40 ]; then
+        icon="󰁽"
+    elif [ "$battery_percent" -gt 30 ]; then
+        icon="󰁼"
+    elif [ "$battery_percent" -gt 20 ]; then
+        icon="󰁻"
+    else
+        icon="󰂃"
+    fi
+
+    echo "$icon $battery_percent %"
+fi
