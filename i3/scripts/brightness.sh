3239f51dcf9d879226cee256b2b3519a78237de8
rishabh-yadav11 <rishabhyadav621@gmail.com>
2024-01-13 22:36:25 +0530

    Update i3 configuration files for volume and brightness control

---
+++ i3/scripts/brightness.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Use brillo to logarithmically adjust laptop screen brightness
+# and send a notification displaying the current brightness level after.
+
+send_notification() {
+	brightness=$(brightnessctl | awk -F '[()]' '/Current brightness/{print $2}')
+	dunstify -a "changebrightness" -u low -r 9993 -h int:value:"$brightness" -i "brightness" "Brightness" "Currently at $brightness%" -t 2000
+}
+
+case $1 in
+up)
+	brightnessctl set +4
+	send_notification "$1"
+	;;
+down)
+  brightnessctl set 4-
+	send_notification "$1"
+	;;
+esac
