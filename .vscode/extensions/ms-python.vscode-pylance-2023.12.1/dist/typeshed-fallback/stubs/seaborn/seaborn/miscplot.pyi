d3df1d4966bf00126ffb13b549cedde11e9597d1
rishabh yadav <75245758+rishabh-yadav11@users.noreply.github.com>
2024-01-23 17:29:07 +0530

    remove bloat

--- .vscode/extensions/ms-python.vscode-pylance-2023.12.1/dist/typeshed-fallback/stubs/seaborn/seaborn/miscplot.pyi
+++
@@ -1,8 +0,0 @@
-from collections.abc import Sequence
-
-from matplotlib.typing import ColorType
-
-__all__ = ["palplot", "dogplot"]
-
-def palplot(pal: Sequence[ColorType], size: int = 1) -> None: ...
-def dogplot(*_, **__) -> None: ...
